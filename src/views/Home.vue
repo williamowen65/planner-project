1370e46be7f52ac264f6df2cbf0e2155f3da5729
William_O <william.owen.bsc@gmail.com>
2020-12-22 08:57:29 -0800

    git commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,41 @@
+
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+          <SingleProject :project="project" @delete="handleDelete"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleProject from '../components/SingleProject'
 
 export default {
   name: 'Home',
-  components: {},
+  components: { SingleProject },
   data() {
     return {
-
+      projects: []
     }
   },
   mounted() {
     fetch('http://localhost:3000/projects')
       .then(res => res.json())
-      .then(data => {})
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id
+      })
+    }
   }
 }
+
+//  All we're doing is grabbing the data in the mounted hook, updating the projects, then when we have
+//  the project, were looping through them and outputting the title for each one.
+
 </script>
